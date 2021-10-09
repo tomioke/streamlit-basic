04dbb934e559fef227e6f3798f1d572832a90b07
unknown <tomiirvan@gmail.com>
2021-10-10 05:48:10 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+st.title("Ini adalah Judul")
+
+st.write("===========================================")
+
+st.header("Ini bagian header")
+
+st.write("===========================================")
+
+st.write("Ini adalah teks biasa")
+
+st.write("===========================================")
+
+st.subheader("Ini adalah subheader")
+
+st.write("===========================================")
+
+"""
+# Ini header
+## Ini subheader
+"""
+
+st.write("===========================================")
+
+# Tampilkan key value
+{
+    "Key": "Value",
+    "Key2": "Value2"
+}
